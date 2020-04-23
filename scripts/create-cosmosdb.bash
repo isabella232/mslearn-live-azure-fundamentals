4f0c3efba45804059108631f2487952ce8755af5
René Ruppert <rene.ruppert@gmail.com>
2020-04-23 16:05:28 +0200

    Add AZ CLI script to create CosmosDB instance

---
+++ scripts/create-cosmosdb.bash
@@ -0,0 +1,40 @@
+# ======================================================================================================
+# This script generates a CosmosDB instance with a database named "maindb" and a container named "data".
+# ======================================================================================================
+
+# Variables
+suffix=$RANDOM
+cosmosDbInstanceName="dbbacktoschool$suffix"
+databaseName="maindb"
+containerName="data"
+location="centralus"
+
+# Only needed when executing locally and not in Cloud Shell
+# echo "Logging in to Learn Live Sandbox - make sure you have activated one at aka.ms/learnlivesandbox. If you haven't, please cancel this script using CTRL+C."
+# az login --tenant learn.docs.microsoft.com
+# printf "\n"
+
+printf "Getting resource group name from sandbox...\n"
+resourceGroup=$(az group list --query '[0].name' --output tsv)
+printf "Resource group: %s\n" $resourceGroup
+printf "\n"
+
+# Set defaults for all following commands
+az configure --defaults group=$resourceGroup
+az configure --defaults location=$location 
+
+printf "Creating a CosmosDB instance named '%s'...this can take long. I mean, really long.\n" $cosmosDbInstanceName
+az cosmosdb create --name $cosmosDbInstanceName
+
+# Create a database.
+printf "Creating a database named '%s' in the instance '%s'\n" $databaseName $cosmosDbInstanceName
+az cosmosdb sql database create --name $databaseName --account-name $cosmosDbInstanceName
+
+# Create a container in the database
+printf "Creating a container named '%s' in database '%s'.\n" $containerName $databaseName
+az cosmosdb sql container create --name $containerName --partition-key-path /id --account-name $cosmosDbInstanceName --database-name $databaseName
+
+# Get the primary connection string
+printf "Getting primary connection string for instance '%s'.\n" $cosmosDbInstanceName
+connectionString=$(az cosmosdb keys list --name $cosmosDbInstanceName --type connection-strings --query 'connectionStrings[0].connectionString' --output tsv)
+printf "Use this connection string:\n\n%s\n\n" $connectionString
